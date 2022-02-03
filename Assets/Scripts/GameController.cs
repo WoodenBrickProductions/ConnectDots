99d4aa9ec3b45345bb53fe90443d1c7bf4f6d3a1
WoodenBrickProductions <tautvisceponis@gmail.com>
2022-02-03 17:31:27 +0200

    Added level loading

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.InputSystem;
 using Touch = UnityEngine.Touch;
 
 
@@ -13,43 +13,86 @@ public class GameController : MonoBehaviour
     [SerializeField] private GameObject pointPrefab;
     [SerializeField] private string pathToLevelData;
     
+    private TouchControl touchControl;
     private List<Point> points;
 
     public LevelCollection levelCollection;
+
+    private void Awake()
+    {
+        touchControl = new TouchControl();
+    }
+
     // Start is called before the first frame update
     void Start()
+    {
+        touchControl.Touch.TouchPress.started += ctx => StartTouch(ctx);
+        touchControl.Touch.TouchPress.canceled += ctx => EndTouch(ctx);
+        points = new List<Point>(); 
+        
+        LoadLevelData();
+        LoadLevel(0);
+    }
+
+    void LoadLevel(int index)
+    {
+        string[] pointData = levelCollection.levels[index].level_data;
+        GameObject container = new GameObject("Container", typeof(RectTransform));
+        container.transform.SetParent(canvas.transform);
+        RectTransform rect = container.GetComponent<RectTransform>();
+        rect.anchorMin = new Vector2(0, 1);
+        rect.anchorMax = new Vector2(0, 1);
+        rect.position = new Vector3(0,0, 0);
+        
+        for (int i = 0; i < pointData.Length/2; i++)
+        {
+            GameObject obj = Instantiate(pointPrefab, container.transform);
+            obj.GetComponent<RectTransform>().position = new Vector3(int.Parse(pointData[i]), Screen.height - int.Parse(pointData[i + 1]), 0);
+            points.Add(obj.GetComponent<Point>());
+        }
+    }
+
+    void LoadLevelData()
     {
         using (StreamReader reader = new StreamReader(pathToLevelData))
         {
             string json = reader.ReadToEnd();
             levelCollection = JsonUtility.FromJson<LevelCollection>(json);
         }
-        
-        // // levelCollection = 
-        //
-        // levelCollection = new LevelCollection();
-        // levelCollection.levels = new Level[2];
-        // levelCollection.levels[0] = new Level();
-        // levelCollection.levels[0].level_data = new string[]{"100", "100", "100", "100"};
-        // levelCollection.levels[1] = new Level();
-        // levelCollection.levels[1].level_data = new string[]{"10", "00", "10", "10"};
-        //
-        
-        using (StreamWriter writer = new StreamWriter("Assets/Resources/Data/test.json"))
-        {
-            string json = JsonUtility.ToJson(levelCollection);
-            writer.Write(json);
-        }
-
-        // LevelCollection levelCollection2 = JsonUtility.FromJson<LevelCollection>("Assets/Resources/Data/test.json");
-        
-        
-        TouchSimulation.Enable();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+    }
+
+    private void OnEnable()
+    {
+        touchControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        touchControl.Disable();
+    }
+
+    private void StartTouch(InputAction.CallbackContext context)
+    {
+        Vector2 touchPosition = touchControl.Touch.TouchPosition.ReadValue<Vector2>();
+        print("Touch started" + touchPosition);
+
+        foreach (var point in points)
+        {
+            if (point.InRadius(touchPosition))
+            {
+                print("Finger near gem");
+            }
+        }
+    }
+
+    private void EndTouch(InputAction.CallbackContext context)
+    {
+        print("Touch ended");
     }
 
     [Serializable]
